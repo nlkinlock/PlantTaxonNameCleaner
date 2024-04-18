833a2c6a1fe0a69e2df241b812ae5557c78a5578
Nicole Kinlock <nlkinlock@gmail.com>
2024-04-18 16:30:32 +0200

    added infra rank check pt 2

--- cleanTaxonNames.py
+++ cleanTaxonNames.py
@@ -11,7 +11,7 @@ import csv
 print("cleanTaxonNames.py: began at", datetime.now().strftime("%H:%M:%S"))
 #
 # !!! uncomment and use only if debugging !!!
-# full_filename = "~/Postdoc/WorldCuP/Sources/Galapagos/Galapagos_Guezou_2010.csv"
+# full_filename = "/media/nlkhd//Postdoc/WorldCuP/Sources/SouthKorea/Cheongju/Cheongju_MidongsanArboretum_2022.csv"
 # old_text_bool = False
 # auth_split_bool = True
 # cv_bool = True
@@ -357,7 +357,8 @@ def split_taxon(this_taxon_init, authority = auth_split_bool, verbose = False):
         else:
             # if authorities included, only split cv if 'Cultivar Name' method is used
             cv_idx_init2 = return_matches(arr = this_taxon, regexp = "^[\u201A\u201B\u201E\u201C\u201F\u201D\u2019\u2018\u0022\u275D\u275E\u2E42\u301D\u301E\u301F\uFF02\u275B\u275C\u275F\u00B4'\"][A-Za-z-0-9À-ÿĀ-ſƀ-ȳ.'\u201B\u2019\u2018\u275C\u00B4]{2,}[\u201A\u201B\u201E\u201C\u201F\u201D\u2019\u2018\u0022\u275D\u275E\u2E42\u301D\u301E\u301F\uFF02\u275B\u275C\u275F\u00B4'\"]$|^[\u201A\u201B\u201E\u201C\u201F\u201D\u2019\u2018\u0022\u275D\u275E\u2E42\u301D\u301E\u301F\uFF02\u275B\u275C\u275F\u00B4'\"][A-Za-z-0-9À-ÿĀ-ſƀ-ȳ.'\u201B\u2019\u2018\u275C\u00B4]+$|^[A-Za-z-0-9À-ÿĀ-ſƀ-ȳ.'\u201B\u2019\u2018\u275C\u00B4]+[\u201A\u201B\u201E\u201C\u201F\u201D\u2019\u2018\u0022\u275D\u275E\u2E42\u301D\u301E\u301F\uFF02\u275B\u275C\u275F\u00B4'\"]$", mode = 'index')
-            cv_idx = [i for i, x in enumerate(this_taxon) if i >= min(cv_idx_init2) and i <= max(cv_idx_init2)]
+            if (len(cv_idx_init2) > 0):
+                cv_idx = [i for i, x in enumerate(this_taxon) if i >= min(cv_idx_init2) and i <= max(cv_idx_init2)]
         this_cv = [e for i, e in enumerate(this_taxon) if i in cv_idx]
         if (len(cv_idx) > 1):
             this_cv = " ".join(this_cv)
